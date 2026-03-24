fc360c40fa720ced331a8c6e677cbc628a3d4f76
Diyarka <240323@astanait.edu.kz>
2026-03-25 02:20:20 +0500

    tree test

---
+++ scripts/game/GameManager.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class GameManager : Node2D
+{
+	public Godot.Collections.Dictionary<string, float> Items = new()
+	{
+		{"Energy", 0},
+		{"Oxygen", 0},
+		{"Carbon", 0},
+		{"Nitrogen", 0},
+		{"Phosphorus", 0},
+		{"Water", 0},
+		{"Sugar", 0},
+		{"Chitin", 0}
+	};
+	
+	public Godot.Collections.Dictionary<string, float> MaxItems = new()
+	{
+		{"Energy", 100},
+		{"Oxygen", 100},
+		{"Carbon", 100},
+		{"Nitrogen",100},
+		{"Phosphorus", 100},
+		{"Water",100},
+		{"Sugar", 100},
+		{"Chitin", 100}
+	};
+	
+	
+
+	public bool CanConsume(Godot.Collections.Dictionary<string, float> count)
+	{
+		foreach (var (key, f) in count)
+			if (Items.TryGetValue(key, out var value))
+				if (value < f)
+					return false;
+		return true;
+	}
+
+	public void Consume(Godot.Collections.Dictionary<string, float> count)
+	{
+		foreach (var (key, f) in count)
+			if (Items.ContainsKey(key))
+				Items[key] -= f;
+	}
+
+	public void Add(Godot.Collections.Dictionary<string, float> count)
+	{
+		foreach (var (key, f) in count)
+			if (Items.TryGetValue(key, out float value))
+				Items[key] = float.Max(value + f, MaxItems[key]);
+	}
+}
--- scripts/trees/Tree.cs
+++ scripts/trees/Tree.cs
@@ -3,14 +3,26 @@ using System;
 
 public partial class Tree : Area2D
 {
-	
+	[Export] private Godot.Collections.Dictionary<int, Rect2> levelCrops = new()
+	{
+
+	};
+	[Export] Sprite2D treeSprite;
+	CollisionShape2D collisionShape;
 	public override void _Ready()
+	{
+		collisionShape = GetNode<CollisionShape2D>("CollisionShape2D");
+	}
+
+	public override void _Process(double delta)
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void Upgrade()
 	{
+		treeSprite.RegionRect = levelCrops[1];
+		var shape = (RectangleShape2D)collisionShape.Shape;
+		shape.Size = levelCrops[1].Size;
 	}
 
 	public virtual void Produce()
